ba8d5ecb6d2ab73736a46547a66b597405c35298
itou <a.itou.sys22@morijyobi.ac.jp>
2025-02-05 11:14:33 +0900

    修正

--- resources/views/schedules/schedule.blade.php
+++ resources/views/schedules/schedule.blade.php
@@ -89,7 +89,7 @@
 
 @section('scripts')
 <script>
-    let schedules = JSON('@json($schedules)'); 
+    let schedules = JSON.parse('@json($schedules)'); 
 </script>
 <script src="{{ asset('js/callender.js') }}"></script>
 @endsection
