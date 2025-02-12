4ac357d8bdeb215ee39e8d82d29e8f830d49e358
itou <a.itou.sys22@morijyobi.ac.jp>
2025-02-12 11:18:31 +0900

    タイトル修正

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Default Title')</title>
+    <title>@yield('title', 'FanFolio')</title>
     <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
     <link rel="stylesheet" href="{{asset('css/scal.css')}}">
         <!-- ページごとのCSS -->
